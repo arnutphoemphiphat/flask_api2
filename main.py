57d3f029c1b26dbc94b1667f447a42a74ea27b4b
arnut1650702747 <arnut.phoe@bumail.net>
2025-02-27 17:25:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, BU 1650702747!'
+    return 'Hello, BU 1650702747'
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
